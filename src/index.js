12d650aa3793306a7b2a47283cd229b4ac24d1db
Tara Ojo <tara.ojo@futurelearn.com>
2018-04-14 12:23:13 +0100

    Fix `react/jsx-filename-extension` eslint violations

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
