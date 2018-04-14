e4334f190a5c63b65c448a6502e1748f845e1833
Tara Ojo <tara.ojo@futurelearn.com>
2018-04-14 12:28:53 +0100

    Fix `import/first` eslint violation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Logo from '../images/safecheck-logo-white.png';
 import { NavLink } from 'react-router-dom';
+import Logo from '../images/safecheck-logo-white.png';
 
 const Home = () => (
   <div>
